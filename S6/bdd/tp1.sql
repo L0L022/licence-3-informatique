3afdfe499b2a31a8de6213b3551f0b5a64f30e7b
Loïc Escales <loic.escales@kakos.fr>
2019-02-04 17:49:48 +0100

    tp bdd toujours plus plus mieux

--- S6/bdd/tp1.sql
+++ S6/bdd/tp1.sql
@@ -86,18 +86,118 @@ END;
 /
 
 CREATE OR REPLACE FUNCTION EnsAtt2Chaine(p_NumEnsAtt INTEGER) RETURN VARCHAR IS
-atts VARCHAR;
+atts VARCHAR(100);
 BEGIN
 
-    FOR att IN (SELECT NomAtt FROM EnsembleContientAtribut ORDER BY NomAtt) LOOP
+    FOR row IN (SELECT NomAtt FROM EnsembleContientAtribut WHERE NumEnsAtt = p_NumEnsAtt ORDER BY NomAtt) LOOP
         IF atts IS NULL THEN
-            atts := att;
+            atts := row.NomAtt;
         ELSE
-            atts := atts || ',' || att;
+            atts := atts || ',' || row.NomAtt;
         END IF;
     END LOOP;
 
     RETURN atts;
+END;
+/
+
+declare atts VARCHAR(100);
+BEGIN atts := EnsAtt2Chaine(37); DBMS_OUTPUT.PUT_LINE(atts);
+END;
+/
+
+CREATE OR REPLACE FUNCTION EstElement(p_NomAtt VARCHAR, p_NumEnsAtt INTEGER) RETURN INTEGER IS
+BEGIN
+    IF INSTR(EnsAtt2Chaine(p_NumEnsAtt), p_NomAtt) = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN 1;
+    END IF;
+END;
+/
+
+declare num3 INTEGER;
+BEGIN Num3 := EstElement('D', 37); DBMS_OUTPUT.PUT_LINE(Num3);
+END;
+/
+
+CREATE OR REPLACE FUNCTION EstInclus(p_NumEnsAtt_petit INTEGER, p_NumEnsAtt_grand INTEGER) RETURN INTEGER IS
+BEGIN
+    FOR row IN (SELECT NomAtt FROM EnsembleContientAtribut WHERE NumEnsAtt = p_NumEnsAtt_petit ORDER BY NomAtt) LOOP
+        IF EstElement(row.NomAtt, p_NumEnsAtt_grand) = 0 THEN
+            RETURN 0;
+        END IF;
+    END LOOP;
+    RETURN 1;
+END;
+/
+
+CREATE OR REPLACE FUNCTION EstEgal(p_NumEnsAtt_1 INTEGER, p_NumEnsAtt_2 INTEGER) RETURN INTEGER IS
+BEGIN
+    IF EnsAtt2Chaine(p_NumEnsAtt_1) = EnsAtt2Chaine(p_NumEnsAtt_2) THEN
+        RETURN 1;
+    ELSE
+        RETURN 0;
+    END IF;
+END;
+/
+
+CREATE OR REPLACE FUNCTION UnionAtt(p_NumEnsAtt_1 INTEGER, p_NumEnsAtt_2 INTEGER) RETURN INTEGER IS
+NumEnsAtt INTEGER;
+BEGIN
+    NumEnsAtt := CreerEnsAttVide();
+
+    FOR row IN (SELECT DISTINCT NomAtt FROM EnsembleContientAtribut WHERE NumEnsAtt = p_NumEnsAtt_1 OR NumEnsAtt = p_NumEnsAtt_2) LOOP
+        AjouterAtt(row.NomAtt, NumEnsAtt);
+    END LOOP;
+    
+    RETURN NumEnsAtt;
+END;
+/
+
+CREATE OR REPLACE FUNCTION IntersectionAtt(p_NumEnsAtt_1 INTEGER, p_NumEnsAtt_2 INTEGER) RETURN INTEGER IS
+NumEnsAtt INTEGER;
+BEGIN
+    NumEnsAtt := CreerEnsAttVide();
+
+    FOR row IN (SELECT NomAtt FROM EnsembleContientAtribut WHERE NumEnsAtt = p_NumEnsAtt_1
+                INTERSECT
+                SELECT NomAtt FROM EnsembleContientAtribut WHERE NumEnsAtt = p_NumEnsAtt_2)
+    LOOP
+        AjouterAtt(row.NomAtt, NumEnsAtt);
+    END LOOP;
+    
+    RETURN NumEnsAtt;
+END;
+/
+
+CREATE OR REPLACE FUNCTION SoustractionAtt(p_NumEnsAtt_1 INTEGER, p_NumEnsAtt_2 INTEGER) RETURN INTEGER IS
+NumEnsAtt INTEGER;
+BEGIN
+    NumEnsAtt := CreerEnsAttVide();
+
+    FOR row IN (SELECT NomAtt FROM EnsembleContientAtribut WHERE NumEnsAtt = p_NumEnsAtt_1
+                MINUS
+                SELECT NomAtt FROM EnsembleContientAtribut WHERE NumEnsAtt = p_NumEnsAtt_2)
+    LOOP
+        AjouterAtt(row.NomAtt, NumEnsAtt);
+    END LOOP;
+    
+    RETURN NumEnsAtt;
+END;
+/
+
+CREATE OR REPLACE FUNCTION CopieAtt(p_NumEnsAtt INTEGER) RETURN INTEGER IS
+NumEnsAtt INTEGER;
+BEGIN
+    NumEnsAtt := CreerEnsAttVide();
+
+    FOR row IN (SELECT NomAtt FROM EnsembleContientAtribut WHERE NumEnsAtt = p_NumEnsAtt)
+    LOOP
+        AjouterAtt(row.NomAtt, NumEnsAtt);
+    END LOOP;
+    
+    RETURN NumEnsAtt;
 END;
 /
 
